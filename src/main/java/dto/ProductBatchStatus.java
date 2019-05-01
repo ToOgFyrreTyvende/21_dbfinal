673ae0f335ff2f0c02797591e2f51103f17e841e
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-05-01 21:39:45 +0200

    Update ProductBatchStatus.java
    Creates link to 'ProductBatch' automatically now

--- src/main/java/dto/ProductBatchStatus.java
+++ src/main/java/dto/ProductBatchStatus.java
@@ -1,26 +1,41 @@
 package dto;
 
 import dto.interfaces.IProductBatchStatus;
+import org.hibernate.annotations.Target;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class ProductBatchStatus implements IProductBatchStatus {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_batch_status_id")
     private int prodBatchStatusId;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @Target(ProductBatch.class)
+    @JoinColumn(name = "product_batch_id")
+    private ProductBatch productBatch;
+
+    public ProductBatchStatus(){
+    }
+
     @Override
-    public int getProdBatchStatusId() {
+    public int getProdBatchStatusId(){
         return prodBatchStatusId;
     }
 
     @Override
-    public void setProdBatchStatusId(int prodBatchStatusId) {
+    public void setProdBatchStatusId(int prodBatchStatusId){
         this.prodBatchStatusId = prodBatchStatusId;
+    }
+
+    public ProductBatch getProductBatch(){
+        return productBatch;
+    }
+
+    public void setProductBatch(ProductBatch productBatch){
+        this.productBatch = productBatch;
     }
 }
 
