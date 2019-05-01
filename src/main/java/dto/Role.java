7c73b914ca066d1e7512ebc3ce37ab1c2d851700
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-05-01 18:43:47 +0200

    Update Role.java
    Not sure how (or if) hibernate knows to connect role table to the external table connecting users and roles, i guess we'll find out!

--- src/main/java/dto/Role.java
+++ src/main/java/dto/Role.java
@@ -3,12 +3,26 @@ package dto;
 import dto.interfaces.IRole;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
+@Table(name = "Role", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "role_id"),
+        @UniqueConstraint(columnNames = "role_name")})
 public class Role implements IRole {
-    @Id @GeneratedValue
-    @Column(name = "role_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", unique = true, nullable = false)
     private int roleId;
+
+    @Column(name = "role_name", unique = true, nullable = false, length = 25)
+    private String roleName;
+
+    @ManyToMany
+    Set<User> users;
+
+    public Role(){
+    }
 
     @Override
     public int getRoleId() {
@@ -18,5 +32,21 @@ public class Role implements IRole {
     @Override
     public void setRoleId(int roleId) {
         this.roleId = roleId;
+    }
+
+    public String getRoleName(){
+        return roleName;
+    }
+
+    public void setRoleName(String roleName){
+        this.roleName = roleName;
+    }
+
+    public Set<User> getUsers(){
+        return users;
+    }
+
+    public void setUsers(Set<User> users){
+        this.users = users;
     }
 }
