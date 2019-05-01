981eb72dc05b2f0bb4eb50b3cdeeabf439b73c23
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-05-01 21:38:18 +0200

    Update Product.java
    Almost done with hibernate work on this class!
    Check that it's join table with users work correctly

--- src/main/java/dto/Product.java
+++ src/main/java/dto/Product.java
@@ -1,25 +1,57 @@
 package dto;
 
 import dto.interfaces.IProduct;
+import org.hibernate.annotations.Target;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
+@Table(name = "Product", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "product_id")})
 public class Product implements IProduct {
-    @Id @GeneratedValue
-    @Column(name = "product_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", unique = true, nullable = false)
     private int prodId;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @Target(Recipe.class)
+    @JoinColumn(name = "product_recipe_id")
+    private Recipe productRecipe;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "product_batches",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_batch_id")})
+    private Set<ProductBatch> prodBatch;
+
+    public Product(){
+    }
+
     @Override
-    public int getProdId() {
+    public int getProdId(){
         return prodId;
     }
 
     @Override
-    public void setProdId(int prodId) {
+    public void setProdId(int prodId){
         this.prodId = prodId;
+    }
+
+    public Recipe getProductRecipe(){
+        return productRecipe;
+    }
+
+    public void setProductRecipe(Recipe productRecipe){
+        this.productRecipe = productRecipe;
+    }
+
+    public Set<ProductBatch> getProdBatch(){
+        return prodBatch;
+    }
+
+    public void setProdBatch(Set<ProductBatch> prodBatch){
+        this.prodBatch = prodBatch;
     }
 }
