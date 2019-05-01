d7a97608fe8dd57eb6e2b77506a79fd14b6cc01a
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-05-01 21:39:23 +0200

    Update ProductBatch.java
    Now creates connections automatically to both 'Product' and 'ProductBatchStatus'
    Need to create link to 'RawMatBatch' Table

--- src/main/java/dto/ProductBatch.java
+++ src/main/java/dto/ProductBatch.java
@@ -1,22 +1,56 @@
 package dto;
 
 import dto.interfaces.IProductBatch;
+import org.hibernate.annotations.Target;
 
 import javax.persistence.*;
 
 @Entity
+@Table(name = "Product_Batch", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "product_batch_id")})
 public class ProductBatch implements IProductBatch {
-    @Id @GeneratedValue
-    @Column(name = "product_batch_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_batch_id", unique = true, nullable = false)
     private int prodBatchId;
 
+    @ManyToOne
+    @JoinTable(name = "product_batches",
+            joinColumns = {@JoinColumn(name = "product_batch_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_id")})
+    private Product product;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @Target(ProductBatchStatus.class)
+    @JoinColumn(name = "batch_status_id")
+    private ProductBatchStatus batchStatus;
+
+    public ProductBatch(){
+    }
+
     @Override
-    public int getProdBatchId() {
+    public int getProdBatchId(){
         return prodBatchId;
     }
 
     @Override
-    public void setProdBatchId(int prodBatchId) {
+    public void setProdBatchId(int prodBatchId){
         this.prodBatchId = prodBatchId;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+
+    public void setProduct(Product product){
+        this.product = product;
+    }
+
+    public ProductBatchStatus getBatchStatus(){
+        return batchStatus;
+    }
+
+    public void setBatchStatus(ProductBatchStatus batchStatus){
+        this.batchStatus = batchStatus;
     }
 }
