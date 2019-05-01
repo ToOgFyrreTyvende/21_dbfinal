5088e8b2833907d9971fdb76a559ef8dd1b25c36
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-05-01 21:42:18 +0200

    Update Recipe.java
    'Recipe' is now automatically linked with 'Product'
    Need to link to 'RecipeHistory' and 'Ingredients'

--- src/main/java/dto/Recipe.java
+++ src/main/java/dto/Recipe.java
@@ -1,22 +1,42 @@
 package dto;
 
 import dto.interfaces.IRecipe;
+import org.hibernate.annotations.Target;
 
 import javax.persistence.*;
 
 @Entity
+@Table(name = "Recipe", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "recipe_id")})
 public class Recipe implements IRecipe {
-    @Id @GeneratedValue
-    @Column(name = "recipe_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_id", unique = true, nullable = false)
     private int recipeId;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @Target(Product.class)
+    @JoinColumn(name = "recipe_product_id")
+    private Product recipeProduct;
+
+    public Recipe(){
+    }
+
     @Override
-    public int getRecipeId() {
+    public int getRecipeId(){
         return recipeId;
     }
 
     @Override
-    public void setRecipeId(int recipeId) {
+    public void setRecipeId(int recipeId){
         this.recipeId = recipeId;
+    }
+
+    public Product getRecipeProduct(){
+        return recipeProduct;
+    }
+
+    public void setRecipeProduct(Product recipeProduct){
+        this.recipeProduct = recipeProduct;
     }
 }
