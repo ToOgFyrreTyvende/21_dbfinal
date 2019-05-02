0ba7fbc8d304294b7807a0cbb458e67cb38d619d
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-05-02 13:16:58 +0200

    Update Ingredient.java
    Created `@Table` annotation defining name and unique constraints
    
    Fixed 'ingredientId's `@GeneratedValue` and `@Column` annotations
    
    Added `@Column` annotation on 'active' attribute
    
    Created `@OneToMany` relation to class 'RawMatBatch', linking to 'ingredients' attribute in said class
    
    Created `@ManyToMany` relation to class 'Recipe', linking to 'recipeIngredients' in said class
    
    Generated:
    - Constructor
    - Getters/Setters

--- src/main/java/dto/Ingredient.java
+++ src/main/java/dto/Ingredient.java
@@ -3,13 +3,28 @@ package dto;
 import dto.interfaces.IIngredient;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
+@Table(name = "Ingredients", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "ingredient_id")})
 public class Ingredient implements IIngredient {
-    @Id @GeneratedValue
-    @Column(name = "ingredient_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ingredient_id", unique = true, nullable = false)
     private int ingredientId;
+
+    @Column(name = "active", unique = false, nullable = false)
     private boolean active;
+
+    @OneToMany(mappedBy = "ingredients", cascade = CascadeType.ALL)
+    private Set<RawMatBatch> recipeRawMatBatch;
+
+    @ManyToMany(mappedBy = "recipeIngredients")
+    private Set<Recipe> recipes;
+
+    public Ingredient(){
+    }
 
     @Override
     public int getIngredientId() {
@@ -29,5 +44,21 @@ public class Ingredient implements IIngredient {
     @Override
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Set<RawMatBatch> getRecipeRawMatBatch(){
+        return recipeRawMatBatch;
+    }
+
+    public void setRecipeRawMatBatch(Set<RawMatBatch> recipeRawMatBatch){
+        this.recipeRawMatBatch = recipeRawMatBatch;
+    }
+
+    public Set<Recipe> getRecipes(){
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes){
+        this.recipes = recipes;
     }
 }
