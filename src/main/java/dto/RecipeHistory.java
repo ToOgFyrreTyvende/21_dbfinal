25798500e4d40b6ddf9fc3673e1bb03d31b29494
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-05-02 13:12:59 +0200

    Update RecipeHistory.java
    Created `@Table` annotation to define table name and unique constraints
    Fixed `@GeneratedValue` and `@Column` annotations
    Created `@ManyToOne` relation to 'Recipe' class using `@JoinColumn`
    Generated:
    - empty constructor
    - getters/setters

--- src/main/java/dto/RecipeHistory.java
+++ src/main/java/dto/RecipeHistory.java
@@ -5,10 +5,20 @@ import dto.interfaces.IRecipeHistory;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "Recipe_History", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "recipe_history_id")})
 public class RecipeHistory implements IRecipeHistory {
-    @Id @GeneratedValue
-    @Column(name = "recipe_history_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_history_id", unique = true, nullable = false)
     private int recipeHistId;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipeHistoryRecipe;
+
+    public RecipeHistory(){
+    }
 
     @Override
     public int getRecipeHistId() {
@@ -18,5 +28,13 @@ public class RecipeHistory implements IRecipeHistory {
     @Override
     public void setRecipeHistId(int recipeHistId) {
         this.recipeHistId = recipeHistId;
+    }
+
+    public Recipe getRecipeHistoryRecipe(){
+        return recipeHistoryRecipe;
+    }
+
+    public void setRecipeHistoryRecipe(Recipe recipeHistoryRecipe){
+        this.recipeHistoryRecipe = recipeHistoryRecipe;
     }
 }
