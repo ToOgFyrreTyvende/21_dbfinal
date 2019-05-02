6dd90f18537e06d1ac298cce3ab819bc751ac1e1
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-05-02 13:10:44 +0200

    Update Recipe.java
    Changed table name to 'Recipes'
    Created:
    - `@OneToMany` relation to 'RecipeHistory' class
    - `@ManyToMany` relation to 'Ingredient' class with `@JoinTable`

--- src/main/java/dto/Recipe.java
+++ src/main/java/dto/Recipe.java
@@ -4,9 +4,10 @@ import dto.interfaces.IRecipe;
 import org.hibernate.annotations.Target;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-@Table(name = "Recipe", uniqueConstraints = {
+@Table(name = "Recipes", uniqueConstraints = {
         @UniqueConstraint(columnNames = "recipe_id")})
 public class Recipe implements IRecipe {
     @Id
@@ -18,6 +19,15 @@ public class Recipe implements IRecipe {
     @Target(Product.class)
     @JoinColumn(name = "recipe_product_id")
     private Product recipeProduct;
+
+    @OneToMany(mappedBy = "recipeHistoryRecipe", cascade = CascadeType.ALL)
+    private Set<RecipeHistory> recipeHistory;
+
+    @ManyToMany
+    @JoinTable(name = "Recipes_Ingredients",
+            joinColumns = {@JoinColumn(name = "recipe_id")},
+            inverseJoinColumns = {@JoinColumn(name = "ingredient_id")})
+    private Set<Ingredient> recipeIngredients;
 
     public Recipe(){
     }
@@ -38,5 +48,21 @@ public class Recipe implements IRecipe {
 
     public void setRecipeProduct(Product recipeProduct){
         this.recipeProduct = recipeProduct;
+    }
+
+    public Set<RecipeHistory> getRecipeHistory(){
+        return recipeHistory;
+    }
+
+    public void setRecipeHistory(Set<RecipeHistory> recipeHistory){
+        this.recipeHistory = recipeHistory;
+    }
+
+    public Set<Ingredient> getRecipeIngredients(){
+        return recipeIngredients;
+    }
+
+    public void setRecipeIngredients(Set<Ingredient> recipeIngredients){
+        this.recipeIngredients = recipeIngredients;
     }
 }
