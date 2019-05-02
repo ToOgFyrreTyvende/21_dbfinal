ee517508ed0f5b03eadc2f208c543f2a838ebadf
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-05-02 13:21:05 +0200

    Update RawMatBatch.java
    Created `@Table` annotation to define name and unique constraints
    
    Fixed `@GeneratedValue` and `@Column` annotations for 'batchId' attribute
    
    Added `@Column` annotation for attributes:
    - 'manufacturerName'
    - 'amount'
    - 'residual'
    
    Created:
    - `@ManyToMany` relation to class 'ProductBatch' using `@JoinTable` annotation
    - `@ManyToOne` relation to class 'Ingredient' using `@JoinColumn` annotation
    
    Generated:
    - Constructor
    - Getters/Setters

--- src/main/java/dto/RawMatBatch.java
+++ src/main/java/dto/RawMatBatch.java
@@ -3,14 +3,41 @@ package dto;
 import dto.interfaces.IRawMatBatch;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
+@Table(name = "Raw_Mat_Batches", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "supplier_batch_id")})
 public class RawMatBatch implements IRawMatBatch {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "supplier_batch_id", unique = true, nullable = false)
     private int batchId;
+
+    @Column(name = "manufacturer_name",
+            unique = false, nullable = false, length = 80)
     private String manufacturerName;
+
+    @Column(name = "amount",
+            unique = false, nullable = false, length = 255)
     private double amount;
+
+    @Column(name = "residual",
+            unique = false, nullable = true, length = 255)
     private boolean residual;
+
+    @ManyToMany
+    @JoinTable(name = "Product_Batches_Raw_Mat_Batches",
+            joinColumns = {@JoinColumn(name = "supplier_batch_id")},
+            inverseJoinColumns = {@JoinColumn(name = "product_batch_id")})
+    private Set<ProductBatch> rawMatProductBatches;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredients;
+
+    public RawMatBatch(){
+    }
 
     @Override
     public int getBatchId() {
@@ -50,5 +77,21 @@ public class RawMatBatch implements IRawMatBatch {
     @Override
     public void setResidual(boolean residual) {
         this.residual = residual;
+    }
+
+    public Set<ProductBatch> getRawMatProductBatches(){
+        return rawMatProductBatches;
+    }
+
+    public void setRawMatProductBatches(Set<ProductBatch> rawMatProductBatches){
+        this.rawMatProductBatches = rawMatProductBatches;
+    }
+
+    public Ingredient getIngredients(){
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient ingredients){
+        this.ingredients = ingredients;
     }
 }
