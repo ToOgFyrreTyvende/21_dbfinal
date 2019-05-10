e398776fb8ef0a37f93c23edfb2e5886a884834a
Anders Høiberg Michaelsen <xephyzone@gmail.com>
2019-05-10 14:37:59 +0200

    Update UserDAOTest.java
    So this test is useless. Just don't use it :ok_hand:

--- src/test/java/dal/UserDAOTest.java
+++ src/test/java/dal/UserDAOTest.java
@@ -1,63 +1,74 @@
 package dal;
 
 import dto.User;
-import dto.interfaces.IRole;
 import dto.interfaces.IUser;
 import hibernate.HibernateUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDAOTest {
     private SessionFactory factory;
-    private Session ses;
-    private IUser testUser1;
-    private UserDAO userDao;
+    private Session session;
+    private IUser testUser;
+    // private UserDAO userDao;
 
-    @BeforeEach
-    void setUp(){
-        userDao = new UserDAO();
-        testUser1 = new User(555, "testmulti",
+    @Test
+    void userDAOtest(){
+        // Create test user
+        // userDao = new UserDAO();
+        testUser = new User(1, "testmulti",
                 "adv", "555555-5555", "spec");
         ArrayList testRoles = new ArrayList();
         testRoles.add(HibernateUtils.getDefaultRoles().get(0));
         testRoles.add(HibernateUtils.getDefaultRoles().get(1));
-        testUser1.setUserRoles(testRoles);
-        userDao.createUser(testUser1);
+        testUser.setUserRoles(testRoles);
+
+        // init session with hibernate
         factory = HibernateUtils.getSessionFactory();
-        ses = factory.openSession();
-        ses.beginTransaction();
-    }
+        session = factory.openSession();
+        session.beginTransaction();
 
-    @AfterEach
-    void tearDown(){
-        IUser deletionUser = ses.get(User.class, testUser1.getUserId());
-        if (deletionUser != null) userDao.deleteUser(deletionUser);
-        ses.getTransaction().commit();
-        ses.close();
-    }
+        // Save user
+        session.save(testUser);
+        session.getTransaction().commit();
+        session.beginTransaction();
 
-    @Test
-    void getUserTest(){
-        IUser retrUser = ses.get(User.class, testUser1.getUserId());
-        assertEquals(testUser1, retrUser);
-    }
+        // Get user and check correct details
+        IUser retrUser = session.get(User.class, testUser.getUserId());
+        session.getTransaction().commit();
+        session.beginTransaction();
+        assertEquals(testUser.getUserId(), retrUser.getUserId());
+        assertTrue(testUser.getUserName().equals(retrUser.getUserName()));
+        assertTrue(testUser.getIni().equals(retrUser.getIni()));
+        assertTrue(testUser.getCpr().equals(retrUser.getCpr()));
+        assertTrue(testUser.getPassword().equals(retrUser.getPassword()));
+        assertFalse(retrUser.getUserRoles().isEmpty());
 
-    @Test
-    void updateUserTest(){
-        IUser testUser2 = testUser1;
-        testUser2.setUserName("updatedTestUser");
-        testUser2.setIni("upd");
-        userDao.updateUser(testUser2);
-        IUser retrUser = ses.get(User.class, testUser2.getUserId());
-        assertNotEquals(testUser1, retrUser);
-        assertEquals(testUser2, retrUser);
+        // Update user and save to db
+        testUser.setUserName("testUpdated");
+        testUser.setIni("upd");
+        session.saveOrUpdate(testUser);
+        session.getTransaction().commit();
+        session.beginTransaction();
+
+        // check user is different
+        assertFalse(retrUser.getUserName().equals(testUser.getUserName()));
+        assertFalse(retrUser.getIni().equals(testUser.getIni()));
+
+        // Delete user in db
+        session.delete(testUser);
+        session.getTransaction().commit();
+        session.beginTransaction();
+
+        // Check user is deleted
+        retrUser = session.get(User.class, testUser.getUserId());
+        session.getTransaction().commit();
+        session.close();
+        assertNull(retrUser);
     }
 }
