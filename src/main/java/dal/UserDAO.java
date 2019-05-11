72054d1616141961d0c7255a93ff1e26f00c0e74
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-05-11 22:09:51 +0200

    Update UserDAO.java
    Fixed all CRUD methods!
    Created JavaDoc for class.
    Created JavaDoc for methods: `createUser` & `getUser`.
    Fixed `isInvalidUser` method; No longer checks given user for a valid id, since database automatically assigns users an id.
    :ok_hand:

--- src/main/java/dal/UserDAO.java
+++ src/main/java/dal/UserDAO.java
@@ -2,84 +2,116 @@ package dal;
 
 import dto.*;
 import dto.interfaces.IUser;
-import hibernate.HibernateUtils;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
+/**
+ * Handles all CRUD operations of Users to the database.
+ * All methods require a Session object given to them,
+ * meaning one must have executed the following code to
+ * use any methods
+ * <pre>{@code
+ *     SessionFactory f = HibernateUtils.getSessionFactory();
+ *     Session s = f.openSession();
+ * }</pre>
+ * After each method, one must remember to close the session,
+ * as the methods will not do this for you!
+ * Example:
+ * <pre>{@code
+ *     s.close();
+ * }</pre>
+ */
 public class UserDAO {
     // CRUD operations on User
     // TODO: (Method that checks if a user is in the database?)
     //  + deleteUser method
-    public void createUser(IUser user){
-        if (!isValidUser(user)){
-            return;
-        }
-        SessionFactory factory = HibernateUtils.getSessionFactory();
-        Session session = factory.openSession();
+
+    /**
+     * Creates a new user in the database, unless given user is invalid.
+     * User is invalid if it has no username, initials, cpr (of correct length),
+     * or password.
+     * Returns <tt>userId</tt> of type <tt>int</tt> unless user is invalid,
+     * in which case it returns <tt>-1</tt>.
+     *
+     * @param session Pre-opened <tt>Session</tt> object
+     * @param user <tt>IUser</tt> object, which should be created in
+     *             the database.
+     *             Will be checked for validity!
+     * @return <tt>int</tt>: auto-generated id of created user
+     */
+    public int createUser(Session session, IUser user){
+        if (isInvalidUser(user)) return -1;
+
         session.beginTransaction();
         // if (session.get(User.class, user.getUserId()) != null){
         //     System.out.println("User already exists in database, aborted...");
-        //     return;
+        //     return -1;
         // }
-        session.save(user);
+        int userId = (Integer) session.save(user);
         session.getTransaction().commit();
-        User checkUser = session.get(User.class, user.getUserId());
-        System.out.println("Saved user \"" + checkUser.getUserName() + "\" to database.");
-        session.close();
+        // session.beginTransaction();
+        // User retrUser = session.get(User.class, userId);
+        // session.getTransaction().commit();
+        // System.out.println("Saved following user to database:\n" + retrUser);
+        return userId;
     }
 
-    public IUser getUser(int userId){
-        SessionFactory factory = HibernateUtils.getSessionFactory();
-        Session session = factory.openSession();
+    /**
+     * Retrieves a user with specified <tt>userId</tt> from database.
+     * Returns <tt>IUser</tt> object if a user is found with given <tt>userId</tt>,
+     * otherwise returns <tt>null</tt> if no user is found
+     * @param session Pre-opened <tt>Session</tt> object
+     * @param userId The id of requested user
+     * @return <tt>IUser</tt> object if user is found, otherwise <tt>null</tt>
+     */
+    public IUser getUser(Session session, int userId){
         session.beginTransaction();
         IUser user = session.get(User.class, userId);
+        // IUser user = session.get(User.class, userId);
         // if (user == null){
         //     System.out.println("No user found with specified id..." +
         //             "\nReturning empty user");
         //     return new User();
         // }
         session.getTransaction().commit();
-        session.close();
-        System.out.println("User found:\n" + user);
+        // System.out.println("User found:\n" + user);
         return user;
     }
 
-    public void updateUser(IUser user){
-        if (!isValidUser(user)){
+    public void updateUser(Session session, IUser user){
+        if (isInvalidUser(user)) return;
+
+        // SessionFactory factory = HibernateUtils.getSessionFactory();
+        // Session session = factory.openSession();
+        IUser oldUser = getUser(session, user.getUserId());
+        if (oldUser == null){
+            System.out.println("No user found, aborting...");
             return;
         }
-        SessionFactory factory = HibernateUtils.getSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
-        IUser oldUser = session.get(User.class, user.getUserId());
-        // if (oldUser == null){
-        //     System.out.println("No user found with given users id, aborting...");
-        //     return;
-        // }
         oldUser = user;
+        session.beginTransaction();
         session.update(oldUser);
         session.getTransaction().commit();
-        session.close();
-        System.out.println("Updated user:\n" + user);
+        // session.close();
+        // System.out.println("Updated user:\n" + user);
     }
 
-    public void deleteUser(IUser user){
-        SessionFactory factory = HibernateUtils.getSessionFactory();
-        Session session = factory.openSession();
+    public void deleteUser(Session session, IUser user){
+        // SessionFactory factory = HibernateUtils.getSessionFactory();
+        // Session session = factory.openSession();
+        IUser oldUser = getUser(session, user.getUserId());
+        if (oldUser == null){
+            System.out.println("No user found, aborting...");
+            return;
+        }
         session.beginTransaction();
-        IUser oldUser = (User) session.get(User.class, user.getUserId());
-        // if (oldUser == null){
-        //     System.out.println("No user found, aborting...");
-        //     return;
-        // }
         session.delete(oldUser);
         session.getTransaction().commit();
-        session.close();
+        // session.close();
     }
 
-    private boolean isValidUser(IUser user){
-        return String.valueOf(user.getUserId()).length() != 0 && user.getUserName().length() != 0 &&
-                user.getIni().length() != 0 && user.getCpr().length() == 11 && user.getPassword().length() != 0;
+    private boolean isInvalidUser(IUser user){
+        return user.getUserName().length() == 0 || user.getIni().length() == 0 ||
+                user.getCpr().length() != 11 || user.getPassword().length() == 0;
     }
 
 
